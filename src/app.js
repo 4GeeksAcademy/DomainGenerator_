f24af6df3b09246c2ba78a8c252477f2633b595f
juancadz <144126426+juancadz@users.noreply.github.com>
2024-01-19 09:31:16 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+// Función para generar combinaciones de nombres de dominio
+  function generateDomainNames(pronoun, adj, noun) {
+    let domainNames = [];
+
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          let domainName = `${pronoun[i]}${adj[j]}${noun[k]}.com`;
+          domainNames.push(domainName);
+        }
+      }
+    }
+
+    return domainNames;
+  }
+
+// Obtener todas las combinaciones de nombres de dominio
+let domainNames = generateDomainNames(pronoun, adj, noun);
+
+// Imprimir los nombres de dominio en la consola
+domainNames.forEach((domain) => {
+  console.log(domain);
+});
+
   console.log("Hello Rigo from the console!");
 };
